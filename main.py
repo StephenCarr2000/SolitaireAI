09dfab1647acb8ec9f6903a77cea841afb10b8f4
Stephen Carr <stephencarr2000@gmail.com>
2021-12-27 22:47:01 -0500

    initial sleep buffer and keyboard interrupt

--- main.py
+++ main.py
@@ -1,29 +1,36 @@
 import time
+
+import pyautogui
 from game_logic import GameLogic
 from game_ui import GameUI
+
+time.sleep(10)
+print("waited")
 
 gl = GameLogic()
 gui = GameUI()
 gui.focusOnEmulatorScreen()
 
 #print('test')
-while 1:
-    print("Updating Game State")
+try:
+    while 1:
+        print("Updating Game State")
 
-    gui.UpdateGameState()
+        gui.UpdateGameState()
     
-    print("Calculating Action")
-    next_action = gl.GetNextAction(gui.gs, gui.gv)
-    #print("Action: " + next_action.name)
-    #print(next_action.cards)
+        print("Calculating Action")
+        next_action = gl.GetNextAction(gui.gs, gui.gv)
+        #print("Action: " + next_action.name)
+        #print(next_action.cards)
     
-    #inpstr = input('Continue?')
-    #if inpstr != "y":
-    #    exit(0)
+        #inpstr = input('Continue?')
+        #if inpstr != "y":
+        #    exit(0)
 
-    gui.ProcessAction(next_action)
+        gui.ProcessAction(next_action)
 
-    #time.sleep(1)
-    #exit(0)
+        #time.sleep(1)
+        #exit(0)
+except: KeyboardInterrupt
 
     
