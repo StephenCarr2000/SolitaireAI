07f6006de434fca1f948f254d8b252146f6f1001
Stephen Carr <stephencarr2000@gmail.com>
2021-12-27 23:39:43 -0500

    outline action tree

---
+++ ActionTree.py
@@ -0,0 +1,49 @@
+from Action import Action
+
+
+class ActionNode:
+    parent = None
+    branches = []
+    action = None
+    badAction = False
+    
+    def __init__(self):
+        self.branches = []
+        self.parent = None
+    def __init__(self, parent):
+        self.branches = []
+        parent = parent
+    def __init__(self, parent, action):
+        self.branches = []
+        self.parent = parent
+        self.action = action
+
+    #create parented branch
+    def newBranch(self):
+        self.branches.append(ActionNode(self, None))
+    def newBranch(self, action):
+        self.branches.append(ActionNode(self, action))
+
+
+    #clears further branches
+    def clear(self):
+        self.branches = []
+    
+    #returns head of the tree
+    def head(self):
+        if(self.parent!=None):
+            p = None
+            while(self.parent!=None):
+                p = self.parent
+            return self.parent
+        return self
+    
+
+        
+
+
+
+
+#class ActionTree(ActionNode):
+#    def clear:
+
